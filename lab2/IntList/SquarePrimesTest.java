a0a2041d3a54c7d9f2ec3a409ea4435dd131d343
toutouzi <1300536186@qq.com>
2024-02-27 02:46:03 +0800

    finish lab2

--- lab2/IntList/SquarePrimesTest.java
+++ lab2/IntList/SquarePrimesTest.java
@@ -17,4 +17,12 @@ public class SquarePrimesTest {
         assertEquals("14 -> 15 -> 16 -> 289 -> 18", lst.toString());
         assertTrue(changed);
     }
+
+    @Test
+    public void testSquarePrimes2() {
+        IntList lst = IntList.of(1,5,3,7,1);
+        boolean changed = IntListExercises.squarePrimes(lst);
+        assertEquals("1 -> 25 -> 9 -> 49 -> 1", lst.toString());
+        assertTrue(changed);
+    }
 }
